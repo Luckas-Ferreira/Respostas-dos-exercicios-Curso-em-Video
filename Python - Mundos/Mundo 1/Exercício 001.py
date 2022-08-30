fa217ae3d8bdcda91a0c0d820478e91f49ef7077
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-08-30 10:40:13 -0300

    exercicio 2 adicionado

---
+++ Python - Mundos/Mundo 1/Exercício 001.py
@@ -0,0 +1,4 @@
+"""
+EXERCÍCIO 001: Olá, Mundo!
+Crie um programa que escreva "Olá, Mundo!" na tela.
+"""
---
+++ Python - Mundos/Mundo 1/Exercício 002.py
@@ -0,0 +1,5 @@
+
+"""
+EXERCÍCIO 002: Respondendo ao Usuário
+Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
+"""
