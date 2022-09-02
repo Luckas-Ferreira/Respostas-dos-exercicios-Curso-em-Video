1c7d1d3191350e95e47e075a701acce525c9df58
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-09-02 12:07:05 -0300

    Exercise 5 complete

--- Java - Mundos/Mundo 1/Exercicio005.java
+++ Java - Mundos/Mundo 1/Exercicio005.java
@@ -2,6 +2,16 @@
  * EXERCÍCIO 005: Antecessor e Sucessor
  * Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor.
  */
-public class Exercicio005 {
-    
+import java.util.Scanner;
+ public class Exercicio005 {
+    public static void main(String[] args) {
+        int num, antecessor, sucessor;
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Digite um número: ");
+        num = teclado.nextInt();
+        teclado.close();
+        antecessor = num - 1;
+        sucessor = num+ 1;
+        System.out.printf("Você digitou o número : %d \nSeu antecessor é o %d e seu sucessor é o %d",num, antecessor, sucessor);
+    }
 }
