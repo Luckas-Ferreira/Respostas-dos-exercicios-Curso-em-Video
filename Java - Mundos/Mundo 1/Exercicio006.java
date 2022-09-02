8e3b9526a334cb7e851c72710d4493e9e3885f4f
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-09-02 13:20:36 -0300

    Exercise 6 complete

--- Java - Mundos/Mundo 1/Exercicio006.java
+++ Java - Mundos/Mundo 1/Exercicio006.java
@@ -2,6 +2,24 @@
  * EXERCÍCIO 006: Dobro, Triplo, Raiz Quadrada
  * Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
  */
-public class Exercicio006 {
-    
+
+import java.util.Scanner;
+ public class Exercicio006 {
+    public static void main(String[] args) {
+        int num, dobro, triplo;
+
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Digite um número: ");
+        num = teclado.nextInt();
+        teclado.close();
+
+        double raizQuadrada = (double) Math.sqrt(num);
+        dobro = num * 2;
+        triplo = num * 3;
+        
+        System.out.println("você digitou o número " + num);
+        System.out.println("Dobro: " + dobro);
+        System.out.println("Triplo: " + triplo);
+        System.out.printf("Raiz Quadrada: %.2f", raizQuadrada);
+    }
 }
