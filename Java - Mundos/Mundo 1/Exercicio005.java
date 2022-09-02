2a43ec9a7345bc7c5fd15760e3ded3a297ac5b9f
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-09-02 11:09:10 -0300

    Exercise 5 adder

---
+++ Java - Mundos/Mundo 1/Exercicio005.java
@@ -0,0 +1,7 @@
+/*
+ * EXERCÍCIO 005: Antecessor e Sucessor
+ * Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor.
+ */
+public class Exercicio005 {
+    
+}
