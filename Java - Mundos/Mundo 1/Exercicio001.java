cd8abad6fdcfefcbf14d9a2041e2681e4cd4c6a4
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-09-02 11:37:16 -0300

    Exercise 1 complete

--- Java - Mundos/Mundo 1/Exercicio001.java
+++ Java - Mundos/Mundo 1/Exercicio001.java
@@ -3,6 +3,8 @@
  * Crie um programa que escreva "Olá, Mundo!" na tela.
  */
 public class Exercicio001 {
-
+    public static void main(String[] args) {
+        System.out.print("Olá, Mundo!");
+    }
 }
 
