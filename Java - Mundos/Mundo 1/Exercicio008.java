1310ea5ab242db605fe1e8b2f3996c4ed4ad1aee
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-09-02 13:31:21 -0300

    Exercise 8 complete

--- Java - Mundos/Mundo 1/Exercicio008.java
+++ Java - Mundos/Mundo 1/Exercicio008.java
@@ -3,6 +3,18 @@
  * EXERCÍCIO 008: Conversor de Medidas
  * Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.
  */
+import java.util.Scanner;
 public class Exercicio008 {
-    
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        float metro, centímetros, milímetros;
+        System.out.print("Digite um valor em metros: ");
+        metro = teclado.nextFloat();
+        teclado.close();
+
+        centímetros = metro * 100;
+        milímetros = metro * 1000;
+        System.out.printf("O valor em centímetros fica: %.2fcm \n", centímetros);
+        System.out.printf("O valor em milímetros fica: %.2fmm \n", milímetros);
+    }
 }
