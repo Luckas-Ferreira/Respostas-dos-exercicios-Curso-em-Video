01ea0cd5d70e1e6378b54b56f443febad33b871c
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-09-02 11:43:43 -0300

    Exercise 2 complete

--- Java - Mundos/Mundo 1/Exercicio002.java
+++ Java - Mundos/Mundo 1/Exercicio002.java
@@ -2,6 +2,14 @@
  * EXERCÍCIO 002: Respondendo ao Usuário
  * Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
  */
-public class Exercicio002 {
-    
+import java.util.Scanner;
+ public class Exercicio002 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Qual seu nome? ");
+        String nome = teclado.nextLine();
+        teclado.close();
+
+        System.out.print("É um prazer te conhecer " + nome);
+    }
 }
