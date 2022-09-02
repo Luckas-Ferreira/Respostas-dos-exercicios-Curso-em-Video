c6e3a981d53902731cc058f6e33f795d3fb81045
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-09-02 11:12:07 -0300

    Exercise 8 adder

---
+++ Java - Mundos/Mundo 1/Exercicio008.java
@@ -0,0 +1,8 @@
+
+/*
+ * EXERCÍCIO 008: Conversor de Medidas
+ * Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.
+ */
+public class Exercicio008 {
+    
+}
