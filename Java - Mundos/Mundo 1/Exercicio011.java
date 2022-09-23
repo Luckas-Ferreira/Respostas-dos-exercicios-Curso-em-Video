3514283bbb5cb1f5edc95ffce29e2f2b74676d4b
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-09-22 21:28:07 -0300

    adder exercise 11

---
+++ Java - Mundos/Mundo 1/Exercicio011.java
@@ -0,0 +1,9 @@
+/*
+ * EXERCÍCIO 011: Pintando Parede
+ * Faça um programa que leia a largura e a altura de uma parede em metros,
+ * calcule a sua área e a quantidade de tinta necessária para pintá-la,
+ * sabendo que cada litro de tinta, pinta uma área de 2 m².
+ */
+public class Exercicio011 {
+    
+}
---
+++ Java - Mundos/Mundo 1/Exercicio012.java
@@ -0,0 +1,8 @@
+/*
+ * EXERCÍCIO 012: Calculando Descontos
+ * Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
+ */
+
+public class Exercicio012 {
+    
+}
