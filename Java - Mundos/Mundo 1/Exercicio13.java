dd79a143a0ef34ab7fdccace952fcb7601c4aa64
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-09-22 21:54:57 -0300

    13 exercise adder

---
+++ Java - Mundos/Mundo 1/Exercicio13.java
@@ -0,0 +1,7 @@
+/*
+ * EXERCÍCIO 013: Reajuste Salarial
+ * Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
+ */
+public class Exercicio13 {
+    
+}
