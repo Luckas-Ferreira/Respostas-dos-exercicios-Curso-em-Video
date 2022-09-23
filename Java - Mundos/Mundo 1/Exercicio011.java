08c219a00c3af47789ca5a7cbff7040bc38578d9
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-09-22 22:05:45 -0300

    13 Exercise adder

--- Java - Mundos/Mundo 1/Exercicio011.java
+++ Java - Mundos/Mundo 1/Exercicio011.java
@@ -4,6 +4,22 @@
  * calcule a sua área e a quantidade de tinta necessária para pintá-la,
  * sabendo que cada litro de tinta, pinta uma área de 2 m².
  */
+import java.util.Scanner;
 public class Exercicio011 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        double largura, altura, area, quantidadeTinta;
+        
+        System.out.print("Largura: ");
+        largura = teclado.nextDouble();
+        System.out.print("Altura: ");
+        altura  = teclado.nextDouble();
+        teclado.close();
+
+        area = largura * altura;
+        quantidadeTinta = area / 2;
+        System.out.printf("Será necessário %.2f latas de tinta.", quantidadeTinta);
+    }
+    
     
 }
