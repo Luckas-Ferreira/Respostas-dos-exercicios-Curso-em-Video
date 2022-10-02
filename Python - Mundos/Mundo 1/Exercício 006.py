95f4067bfd04742a11e444cb20f9800634600816
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-10-02 17:26:59 -0300

    Exercise 6 completed

--- Python - Mundos/Mundo 1/Exercício 006.py
+++ Python - Mundos/Mundo 1/Exercício 006.py
@@ -1,4 +1,11 @@
+import math
 """
 EXERCÍCIO 006: Dobro, Triplo, Raiz Quadrada
 Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
 """
+
+nu = int(input("Digite um número: "))
+do = nu * 2
+tr = nu * 3
+rq = math.sqrt(nu)
+print(f"O dobro é {do}, o triplo é {tr} e a raiz quadrada é {rq}")
