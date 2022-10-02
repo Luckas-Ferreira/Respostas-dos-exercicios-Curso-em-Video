d7c7a283c9b29114884883389d68d9018ae0ab7e
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-10-02 17:31:37 -0300

    Exercise 8 completed

--- Python - Mundos/Mundo 1/Exercício 008.py
+++ Python - Mundos/Mundo 1/Exercício 008.py
@@ -1,5 +1,11 @@
 
 """
 EXERCÍCIO 008: Conversor de Medidas
-Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.
+Escreva um programa que leia um valor em metros e o exiba convertido em centímetros 
+e milímetros.
 """
+
+v = float(input("Digite um valor em M: "))
+c = v * 100
+m = v * 1000
+print(f"O valor {v}m em cm fica {c} e em mm fica {m}")
