3e68b231d6ad1fc704edc8c006b69bebb12a57fa
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-10-02 17:41:03 -0300

    Exercise 11 completed

--- Python - Mundos/Mundo 1/Exercício 011.py
+++ Python - Mundos/Mundo 1/Exercício 011.py
@@ -4,3 +4,9 @@ Faça um programa que leia a largura e a altura de uma parede em metros,
 calcule a sua área e a quantidade de tinta necessária para pintá-la,
 sabendo que cada litro de tinta, pinta uma área de 2 m².
 '''
+
+lar = float(input("Largura: "))
+alt = float(input("Altura:  "))
+area = lar * alt
+tinta = area / 2
+print(f"{lar} X {alt} \nArea {area}\nVai precisar de {tinta :.2f} lata de tinta para pintar essa parede")
