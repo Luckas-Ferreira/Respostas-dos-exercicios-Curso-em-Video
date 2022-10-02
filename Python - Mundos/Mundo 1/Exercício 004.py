ad815eb9153d503db335ae47d9a30a20abd3079d
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-10-02 17:24:38 -0300

    exercise 5 completed

--- Python - Mundos/Mundo 1/Exercício 004.py
+++ Python - Mundos/Mundo 1/Exercício 004.py
@@ -4,3 +4,4 @@ EXERCÍCIO 004: Dissecando uma Variável
 Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo
 e todas as informações possíveis sobre ele.
 """
+
--- Python - Mundos/Mundo 1/Exercício 005.py
+++ Python - Mundos/Mundo 1/Exercício 005.py
@@ -2,3 +2,7 @@
 EXERCÍCIO 005: Antecessor e Sucessor
 Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor.
 """
+n1 = int(input("Digite um numero: "))
+su = n1 + 1
+an = n1 - 1
+print(f"O numero {n1} tem como antecessor o {an} e sucessor {su}")
