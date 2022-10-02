aa5cd7d83b2b59f780f0b83d93c3ae164b474d45
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-10-02 17:28:42 -0300

    exercise 7 completed

--- Python - Mundos/Mundo 1/Exercício 007.py
+++ Python - Mundos/Mundo 1/Exercício 007.py
@@ -2,3 +2,8 @@
 EXERCÍCIO 007: Média Aritmética
 Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
 """
+
+n1 = float(input("Primeira nota: "))
+n2 = float(input("Segunda nota: "))
+m = (n1 + n2) / 2
+print(f"A média aritmética entre {n1} e {n2} é: {m}")
