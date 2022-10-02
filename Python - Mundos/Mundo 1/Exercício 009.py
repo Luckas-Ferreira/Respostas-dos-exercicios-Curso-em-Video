e0f7d2ccf6b057ebaeecef459a97d09b57cfb3c0
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-10-02 17:34:58 -0300

    Exercise 9 completed

--- Python - Mundos/Mundo 1/Exercício 009.py
+++ Python - Mundos/Mundo 1/Exercício 009.py
@@ -1,5 +1,16 @@
 
 """
-EXERCÍCIO 008: Conversor de Medidas
-Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.
+EXERCÍCIO 009: Tabuada
+Faça um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada.
 """
+v = int(input("Digite um valor: "))
+print(f"{v} x {1} = {v * 1}")
+print(f"{v} x {2} = {v * 2}")
+print(f"{v} x {3} = {v * 3}")
+print(f"{v} x {4} = {v * 4}")
+print(f"{v} x {5} = {v * 5}")
+print(f"{v} x {6} = {v * 6}")
+print(f"{v} x {7} = {v * 7}")
+print(f"{v} x {8} = {v * 8}")
+print(f"{v} x {9} = {v * 9}")
+print(f"{v} x {10} = {v * 10}")
