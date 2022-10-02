be24bd5f682c97806bb110cfb619328b26f36ac7
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-10-02 17:13:26 -0300

    exercise 3 finished

--- Python - Mundos/Mundo 1/Exercício 003.py
+++ Python - Mundos/Mundo 1/Exercício 003.py
@@ -2,3 +2,7 @@
 EXERCÍCIO 003: Somando Dois Números
 Crie um programa que leia dois números e mostre a soma entre eles.
 """
+n1 = int(input("Primeiro valor: "))
+n2 = int(input("Segundo valor: "))
+s = n1 + n2
+print(f"A soma entre {n1} e {n2} é: {s}")
