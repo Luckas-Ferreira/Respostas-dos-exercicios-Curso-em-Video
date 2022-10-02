9de6b884debfce7ecd4af8505c8909a5de560196
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-10-02 17:11:42 -0300

    exercise 2 adder

--- Java - Mundos/Mundo 1/Exercicio013.java
+++ Java - Mundos/Mundo 1/Exercicio013.java
@@ -2,6 +2,6 @@
  * EXERCÍCIO 013: Reajuste Salarial
  * Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
  */
-public class Exercicio13 {
+public class Exercicio013 {
     
 }
--- Python - Mundos/Mundo 1/Exercício 002.py
+++ Python - Mundos/Mundo 1/Exercício 002.py
@@ -3,3 +3,5 @@
 EXERCÍCIO 002: Respondendo ao Usuário
 Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
 """
+nome = input("Qual seu nome? ")
+print("Prazer em te conhecer " + nome)
