819d09326da1eddbe45a6b8d028c1a90781332aa
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-10-02 17:37:28 -0300

    exercise 10 completed

--- Python - Mundos/Mundo 1/Exercício 010.py
+++ Python - Mundos/Mundo 1/Exercício 010.py
@@ -4,3 +4,7 @@ Crie um programa que leia quanto dinheiro uma pessoa tem na carteira
 e mostre quantos Dólares ela pode comprar.
 Considere U$ 1,00 = R$ 3,27
 """
+
+d = float(input("Digite um valor em reais: "))
+c = d / 3.27
+print(f"Com R${d} dá pra comprar cerca de ${c}")
