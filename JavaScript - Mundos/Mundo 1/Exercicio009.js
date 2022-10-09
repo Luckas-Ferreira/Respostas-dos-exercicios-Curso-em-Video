1fd7d377a128a75403110848e734c4de242d9857
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-10-09 15:19:16 -0300

    Exercise 9 complete

--- JavaScript - Mundos/Mundo 1/Exercicio009.js
+++ JavaScript - Mundos/Mundo 1/Exercicio009.js
@@ -2,3 +2,15 @@
  * EXERCÍCIO 009: Tabuada
  * Faça um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada.
  */
+
+var num = parseInt(prompt('Digite um número: '))
+document.write(`${num} X 1 = ${num * 1} <br>`)
+document.write(`${num} X 2 = ${num * 2} <br>`)
+document.write(`${num} X 3 = ${num * 3} <br>`)
+document.write(`${num} X 4 = ${num * 4} <br>`)
+document.write(`${num} X 5 = ${num * 5} <br>`)
+document.write(`${num} X 6 = ${num * 6} <br>`)
+document.write(`${num} X 7 = ${num * 7} <br>`)
+document.write(`${num} X 8 = ${num * 8} <br>`)
+document.write(`${num} X 9 = ${num * 9} <br>`)
+document.write(`${num} X 10 = ${num * 10}`)
