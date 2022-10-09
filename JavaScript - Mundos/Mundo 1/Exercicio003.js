3c899ec24bb7236bd9196d3d1199bdbab14d6f45
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-10-09 14:18:07 -0300

    Exercise 3 complete

--- JavaScript - Mundos/Mundo 1/Exercicio003.js
+++ JavaScript - Mundos/Mundo 1/Exercicio003.js
@@ -2,3 +2,8 @@
  * EXERCÍCIO 003: Somando Dois Números
  * Crie um programa que leia dois números e mostre a soma entre eles.
  */
+
+var n1 = Number(prompt('Primeiro número: '))
+var n2 = Number(prompt('Segundo número: '))
+var soma = n1 + n2 
+document.write(`A soma entre ${n1} e ${n2} é: ${soma}`)
