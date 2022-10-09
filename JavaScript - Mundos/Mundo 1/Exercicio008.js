f00fc292a63cdf4da15779b243febacc5871842b
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-10-09 14:47:01 -0300

    Exercise 8 complete

--- JavaScript - Mundos/Mundo 1/Exercicio008.js
+++ JavaScript - Mundos/Mundo 1/Exercicio008.js
@@ -2,3 +2,8 @@
  * EXERCÍCIO 008: Conversor de Medidas
  * Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.
  */
+
+var metro = parseFloat(prompt('Digite um valor em metros: '))
+var centímetros = metro * 100
+var milímetros  = metro * 1000
+document.write(`O valor ${metro} em centímetros fica: ${centímetros.toFixed(2)}cm, e em milímetros fica: ${milímetros.toFixed(2)}mm`)
