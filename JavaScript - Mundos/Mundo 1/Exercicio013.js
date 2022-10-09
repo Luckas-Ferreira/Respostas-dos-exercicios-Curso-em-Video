67a934477aa1f5b3101c7ab4bd0bf837e327e810
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-10-09 15:42:54 -0300

    Exercise 13 complete

--- JavaScript - Mundos/Mundo 1/Exercicio013.js
+++ JavaScript - Mundos/Mundo 1/Exercicio013.js
@@ -2,3 +2,7 @@
  * EXERCÍCIO 013: Reajuste Salarial
  * Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
  */
+
+var salário = parseFloat(prompt('Salário: R$'))
+var aumento = salário + (salário * 0.15)
+document.write(`Salário atual: R$${salário.toFixed(2)} <br>Salário com 15% de aumento fica: R$${aumento.toFixed(2)}`)
