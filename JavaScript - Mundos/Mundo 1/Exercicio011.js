1cd0f47a417a2582b588bb0f9b9a39020a931e43
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-10-09 15:34:48 -0300

    Exercise 11 complete

--- JavaScript - Mundos/Mundo 1/Exercicio011.js
+++ JavaScript - Mundos/Mundo 1/Exercicio011.js
@@ -4,3 +4,10 @@
  * calcule a sua área e a quantidade de tinta necessária para pintá-la,
  * sabendo que cada litro de tinta, pinta uma área de 2 m².
  */
+
+var largura = parseFloat(prompt('Largura: '))
+var altura = parseFloat(prompt('Altura: '))
+var area   = (largura * altura)
+var quant_Tinta = area / 2
+document.write(`ÁREA: ${area} <br>Com essas dimensões será necessario cerca de ${quant_Tinta} litros de tinta para pintar essa parede.`)
+
