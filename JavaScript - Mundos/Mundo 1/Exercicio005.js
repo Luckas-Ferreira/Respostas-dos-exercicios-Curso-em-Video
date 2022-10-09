51a74bc1508aeac67476c27beb7f3636078b4835
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-10-09 14:29:22 -0300

    Exercise 6 complete

--- JavaScript - Mundos/Mundo 1/Exercicio005.js
+++ JavaScript - Mundos/Mundo 1/Exercicio005.js
@@ -2,3 +2,8 @@
  * EXERCÍCIO 005: Antecessor e Sucessor
  * Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor.
  */
+
+var num = Number.parseInt(prompt('Digite um número: '))
+var antecessor = num - 1
+var sucessor   = num + 1
+document.write(`O número ${num} tem como antecessor o ${antecessor} e seu sucessor é o ${sucessor}`)
