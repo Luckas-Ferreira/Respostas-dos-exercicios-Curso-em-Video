2b25a7a24d4ef06b729b7f684c64adb68e351d03
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-10-09 15:24:14 -0300

    Exercise 10 complete

--- JavaScript - Mundos/Mundo 1/Exercicio010.js
+++ JavaScript - Mundos/Mundo 1/Exercicio010.js
@@ -2,5 +2,9 @@
  *EXERCÍCIO 010: Conversor de Moedas
  * Crie um programa que leia quanto dinheiro uma pessoa tem na carteira
  * e mostre quantos Dólares ela pode comprar.
- * Considere U$ 1,00 = R$ 3,27
+ * Considere U$ 1,00 = R$ 5,20
  */
+
+var din = parseFloat(prompt('Quanto de dinheiro você tem? R$'))
+var dolar = (din / 5,20)
+document.write(`Com R$${din.toFixed(2)} dá para comprar $${dolar.toFixed(2)} dolares`)
