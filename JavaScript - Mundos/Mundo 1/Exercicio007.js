2d783bd5c1f62fd2267e1a77a0e17452bbba863b
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-10-09 14:42:19 -0300

    Exercise 7 complete

--- JavaScript - Mundos/Mundo 1/Exercicio007.js
+++ JavaScript - Mundos/Mundo 1/Exercicio007.js
@@ -2,3 +2,8 @@
  * EXERCÍCIO 007: Média Aritmética
  * Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
  */
+
+var n1 = Number.parseFloat(prompt('Primeira nota: '))
+var n2 = Number.parseFloat(prompt('Segunda nota: '))
+var media = (n1 + n2) / 2
+document.write(`A media entre ${n1} e ${n2} é igual a ${media}`)
