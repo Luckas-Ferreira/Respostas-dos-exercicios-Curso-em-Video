420b09578a512d210230a55bc3882b4ac2bdf05b
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-10-09 14:33:04 -0300

    Exercise 6 complete

--- JavaScript - Mundos/Mundo 1/Exercicio006.js
+++ JavaScript - Mundos/Mundo 1/Exercicio006.js
@@ -2,3 +2,8 @@
  * EXERCÍCIO 006: Dobro, Triplo, Raiz Quadrada
  * Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
  */
+
+var num = Number(prompt('Digite um número: '))
+var dobro = num * 2
+var triplo = num * 3
+document.write(`O número é o ${num}, o dobro é ${dobro} e o triplo é ${triplo}`)
