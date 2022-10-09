6066e19a587c094d1d9823dbaa8efb41981011f0
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-10-09 14:26:26 -0300

    Exercise 4 complete

--- JavaScript - Mundos/Mundo 1/Exercicio004.js
+++ JavaScript - Mundos/Mundo 1/Exercicio004.js
@@ -3,3 +3,10 @@
  * Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo
  * e todas as informações possíveis sobre ele.
  */
+
+var frase = prompt('Digite algo: ')
+document.write(`É do tipo: ${typeof(frase)} <br>`)
+document.write(`É um uma frase: ${isNaN(frase)} <br>`)
+document.write(`Tem ${frase.length} letras <br>`)
+document.write(`Em maiusculo: ${frase.toUpperCase()} <br>`)
+document.write(`Em minusculo: ${frase.toLowerCase()} <br>`)
