970031c189e40d42099d06fd98b3ec70b648a3fd
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-10-09 15:39:03 -0300

    Exercise 12 complete

--- JavaScript - Mundos/Mundo 1/Exercicio012.js
+++ JavaScript - Mundos/Mundo 1/Exercicio012.js
@@ -2,3 +2,7 @@
  * EXERCÍCIO 012: Calculando Descontos
  * Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
  */
+
+var preço = parseFloat(prompt('Valor do produto: '))
+var desconto = preço - (preço * 0.05)
+document.write(`Preço original: R$${preço.toFixed(2)} <br>Com 5% de desconto: R$${desconto.toFixed(2)}`)
