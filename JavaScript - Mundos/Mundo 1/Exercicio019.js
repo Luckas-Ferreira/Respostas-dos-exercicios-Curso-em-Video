78697403b813457c3d176522486fe328f6a0a14e
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-10-09 21:49:06 -0300

    Adder exercise 19

---
+++ JavaScript - Mundos/Mundo 1/Exercicio019.js
@@ -0,0 +1,5 @@
+/*
+ * EXERCÍCIO 019: Sorteando um Item na Lista
+ * Um professor quer sortear um dos seus quatros alunos para apagar o quadro.
+ * Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome do escolhido.
+ */
