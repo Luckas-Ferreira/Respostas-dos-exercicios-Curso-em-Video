8fd2aa7b95ef75ee69e5bd912075dc4da44e1129
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-10-09 21:42:25 -0300

    Adder exercise 15

---
+++ JavaScript - Mundos/Mundo 1/Exercise015.js
@@ -0,0 +1,6 @@
+/*
+ * EXERCÍCIO 015: Aluguel de Carros
+ * Escreva um programa que pergunte a quantidade de km percorridos por um carro alugado
+ * e a quantidade de dias pelos quais ele foi alugado. Calcule o preço a pagar,
+ * sabendo que o carro custa R$ 60 por dia e R$ 0,15 por km rodado.
+ */
