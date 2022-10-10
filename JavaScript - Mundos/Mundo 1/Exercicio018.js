416778d6f30e643517d45fd8c27d292ddf2f4a09
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-10-09 22:34:26 -0300

    Exercise 18 complete

--- JavaScript - Mundos/Mundo 1/Exercicio018.js
+++ JavaScript - Mundos/Mundo 1/Exercicio018.js
@@ -2,3 +2,9 @@
  * EXERCÍCIO 018: Seno, Cosseno e Tangente
  * Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e tangente desse ângulo. 
  */
+
+var angulo = Number(prompt('Digite um ângulo: '))
+var seno = Math.sin(angulo)
+var cosseno = Math.cos(angulo)
+var tangente = Math.tan(angulo)
+document.write(`O ângulo ${angulo} tem as seguintes informaçẽos: <br> Seno: ${seno.toFixed(2)} <br> Cosseno: ${cosseno.toFixed(2)} <br> Tangente: ${tangente.toFixed(2)}`)
