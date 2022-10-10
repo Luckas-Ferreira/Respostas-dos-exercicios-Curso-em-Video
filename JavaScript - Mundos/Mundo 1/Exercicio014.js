5228262bb3571b8d3b5d846f23a128b8d699a882
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-10-09 22:03:08 -0300

    Exercise 14 complete

--- JavaScript - Mundos/Mundo 1/Exercicio014.js
+++ JavaScript - Mundos/Mundo 1/Exercicio014.js
@@ -1,3 +1,7 @@
 /* EXERCÍCIO 014: Conversor de Temperaturas
  * Escreva um programa que converta uma temperatura digitada em ºC e converta para ºF.
  */
+
+var temp = parseFloat(prompt('Digite uma temperatura em ºC: '))
+var conversão = (temp * (9 / 5)) + 32
+document.write(`ºC ${temp.toFixed(2)} graus Celsius para Fahrenheit fica ºF ${conversão.toFixed(2)}`)
