a21b2a7d77fa9aa29bc331aecd3142a6fc004329
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-10-09 21:50:24 -0300

    Adder exercise 20

---
+++ JavaScript - Mundos/Mundo 1/Exercicio021.js
@@ -0,0 +1,5 @@
+/*
+ * EXERCÍCIO 020: Sorteando uma Ordem na Lista
+ * O mesmo professor do exercício anterior quer sortear a ordem de apresentação de trabalhos dos alunos.
+ * Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
+ */
