a64f8bc0a783c5dfe639dc3900e5f1c5c41bc5e8
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-10-09 22:10:32 -0300

    Exercise 15 complete

--- JavaScript - Mundos/Mundo 1/Exercicio015.js
+++ JavaScript - Mundos/Mundo 1/Exercicio015.js
@@ -4,3 +4,10 @@
  * e a quantidade de dias pelos quais ele foi alugado. Calcule o preço a pagar,
  * sabendo que o carro custa R$ 60 por dia e R$ 0,15 por km rodado.
  */
+
+var km = parseFloat(prompt('KM percorridos pelo carro: '))
+var dias = parseInt(prompt('Quantidade de dias: '))
+var valor_a_Pagar = (km * 0.15) + (dias * 60)
+document.write(`Custo por KM rodado: R$${km * 0.15} <br>`)
+document.write(`Custo por dias alugados: R$${dias * 60} <br>`)
+document.write(`O preço a pagar é de: R$${valor_a_Pagar} <br>`)
