a2e5417a11724369d4bcf16a6182e9d30893c7a1
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-10-12 05:28:47 -0300

    Exercise 19

--- JavaScript - Mundos/Mundo 1/Exercicio019.js
+++ JavaScript - Mundos/Mundo 1/Exercicio019.js
@@ -3,3 +3,8 @@
  * Um professor quer sortear um dos seus quatros alunos para apagar o quadro.
  * Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome do escolhido.
  */
+
+var aluno1 = prompt("Primeiro aluno: ")
+var aluno2 = prompt("Segundo aluno: ")
+var aluno3 = prompt("Terceiro aluno: ")
+var aluno4 = prompt("Quarto aluno: ")
