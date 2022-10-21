b988384b7893ee7a9a63eeb9fd0d73b263daebfa
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-10-21 06:53:12 -0300

    Exercise 19 complete

--- JavaScript - Mundos/Mundo 1/Exercicio019.js
+++ JavaScript - Mundos/Mundo 1/Exercicio019.js
@@ -4,7 +4,13 @@
  * Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome do escolhido.
  */
 
+random = Math.random()
+random = Math.floor(random * 4)
+
 var aluno1 = prompt("Primeiro aluno: ")
 var aluno2 = prompt("Segundo aluno: ")
 var aluno3 = prompt("Terceiro aluno: ")
 var aluno4 = prompt("Quarto aluno: ")
+var alunos = [aluno1, aluno2, aluno3, aluno4]
+var escolhido = alunos[random]
+document.write(`O aluno escolhido para apagar o quadro foi o: ${escolhido}`)
